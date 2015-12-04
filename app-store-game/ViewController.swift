2f5a1123882193105c1bae206a1d4b5e0365dceb
ori mizrachi <orim553@gmail.com>
2015-12-04 23:26:43 +0200

    Initial Commit

---
+++ app-store-game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-store-game
+//
+//  Created by ori mizrachi on 12/4/15.
+//  Copyright © 2015 B-Up!. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
