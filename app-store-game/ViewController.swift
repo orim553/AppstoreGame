0562da7fc7f5a1e8e3abe6dce67cae7335d40920
ori mizrachi <orimizrachi@oris-MacBook-Air.local>
2015-12-07 17:58:51 +0200

    now astroids are kind of good

--- app-store-game/ViewController.swift
+++ app-store-game/ViewController.swift
@@ -9,15 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    // timer stuff
     var astroTimer : NSTimer!
-    var astroCounter = 0
+    var astroCounter : Double = 0
+    var normalTimer : NSTimer!
+    var TimerCounter = 0
 
+
+    @IBOutlet weak var lblHp: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //timer
         astroTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "asteroTimer", userInfo: nil, repeats: true)
+        normalTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "Timer", userInfo: nil, repeats: true)
+        
 
         //swipes
         let leftSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
@@ -32,50 +38,52 @@ class ViewController: UIViewController {
         ivRocket.image = UIImage(named: "purerocket")
         ivRocket.tag = 1
         self.view.addSubview(ivRocket)
+    }//viewdidload
+    
+    func Timer()
+    {
+        TimerCounter++
     }
     
+    //timer function
     func asteroTimer()
     {
-        astroCounter++
+        astroCounter+=1.7
         
-            if astroCounter % 2 == 0
-            {
                 var frame : CGRect = CGRectMake(133, 30, 95, 95)
-                let dice : UInt32 = arc4random_uniform(3) + 1
-                if dice == 1
-            {
-                frame = CGRectMake(1, -20, 95, 95)
-            }
+                if self.view.viewWithTag(1)?.frame.origin.x == 3
+                {
+                    frame = CGRectMake(1, -90, 95, 95)
+                }//else if
         
-            else if dice == 2
-            {
-                frame = CGRectMake(123, -20, 95, 95)
-
-            }
-            else if dice == 3
-            {
-                frame = CGRectMake(230, -20, 95, 95)
-            }
+                else if self.view.viewWithTag(1)?.frame.origin.x == 133
+                {
+                    frame = CGRectMake(123, -90, 95, 95)
+                }//else if
+                    
+                else if self.view.viewWithTag(1)?.frame.origin.x == 263
+                {
+                    frame = CGRectMake(230, -90, 95, 95)
+                }//else if
+                let ivAstro : UIImageView = UIImageView(frame: frame)
+                ivAstro.image = UIImage(named: "aestroid_brown")
+                ivAstro.tag = 10
+                self.view.addSubview(ivAstro)
+                UIView.animateWithDuration(4.0, animations: {ivAstro.frame.origin.y = 600})
         
-            let ivAstro : UIImageView = UIImageView(frame: frame)
-            ivAstro.image = UIImage(named: "aestroid_brown")
-            ivAstro.tag = 10
-            self.view.addSubview(ivAstro)
-            UIView.animateWithDuration(4.0, animations: {ivAstro.frame.origin.y = 600})
-        }
-    }
+    }//asteroTimer
     
+    //swipe function
     func handleSwipes(sender: UISwipeGestureRecognizer) {
-        
         if (sender.direction == .Left)
         {
             UIView.animateWithDuration(0.5, animations: {self.view.viewWithTag(1)?.frame = CGRectMake((self.view.viewWithTag(1)?.frame.origin.x)! - 130, (self.view.viewWithTag(1)?.frame.origin.y)!, (self.view.viewWithTag(1)?.frame.size.width)!, (self.view.viewWithTag(1)?.frame.size.height)!)})
-        }
+        }//if left
         
         if (sender.direction == .Right)
         {
             UIView.animateWithDuration(0.5, animations: {self.view.viewWithTag(1)?.frame = CGRectMake((self.view.viewWithTag(1)?.frame.origin.x)! + 130, (self.view.viewWithTag(1)?.frame.origin.y)!, (self.view.viewWithTag(1)?.frame.size.width)!, (self.view.viewWithTag(1)?.frame.size.height)!)})
-        }
-    }
-}
+        }//if right
+    }//handleswipes
+}//class
 
