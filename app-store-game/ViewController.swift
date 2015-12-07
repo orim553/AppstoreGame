23350442fec06f3afe471fba71ed7686cef3d611
ori mizrachi <orimizrachi@oris-MacBook-Air.local>
2015-12-07 16:52:44 +0200

    added astroids and astroids movement

--- app-store-game/ViewController.swift
+++ app-store-game/ViewController.swift
@@ -11,11 +11,13 @@ import UIKit
 class ViewController: UIViewController {
     
     var astroTimer : NSTimer!
+    var astroCounter = 0
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //timer
-        //astroTimer = NSTimer.scheduledTimerWithTimeInterval(2.5, target: self, selector: "asteroTimer", userInfo: nil, repeats: true)
+        astroTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "asteroTimer", userInfo: nil, repeats: true)
 
         //swipes
         let leftSwipe = UISwipeGestureRecognizer(target: self, action: Selector("handleSwipes:"))
@@ -30,6 +32,37 @@ class ViewController: UIViewController {
         ivRocket.image = UIImage(named: "purerocket")
         ivRocket.tag = 1
         self.view.addSubview(ivRocket)
+    }
+    
+    func asteroTimer()
+    {
+        astroCounter++
+        
+            if astroCounter % 2 == 0
+            {
+                var frame : CGRect = CGRectMake(133, 30, 95, 95)
+                let dice : UInt32 = arc4random_uniform(3) + 1
+                if dice == 1
+            {
+                frame = CGRectMake(1, -20, 95, 95)
+            }
+        
+            else if dice == 2
+            {
+                frame = CGRectMake(123, -20, 95, 95)
+
+            }
+            else if dice == 3
+            {
+                frame = CGRectMake(230, -20, 95, 95)
+            }
+        
+            let ivAstro : UIImageView = UIImageView(frame: frame)
+            ivAstro.image = UIImage(named: "aestroid_brown")
+            ivAstro.tag = 10
+            self.view.addSubview(ivAstro)
+            UIView.animateWithDuration(4.0, animations: {ivAstro.frame.origin.y = 600})
+        }
     }
     
     func handleSwipes(sender: UISwipeGestureRecognizer) {
